eeebd4fecc7d4b1219bfefecebdd7e58da9a007b
ZATA-N <zigbezatanarcisse@gmail.com>
2024-09-23 22:26:30 +0000

    Add files via upload

--- dvdstore/src/main/java/com/mycompany/service/MovieService.java
+++ dvdstore/src/main/java/com/mycompany/service/MovieService.java
@@ -1,14 +1,22 @@
 package com.mycompany.service;
 
 import com.mycompany.entity.Movie;
+import com.mycompany.repository.GoLiveMovieRepository;
 import com.mycompany.repository.MovieRepository;
+
+import java.io.IOException;
 
 public class MovieService {
 
     private MovieRepository movieRepository = new MovieRepository();
+    private GoLiveMovieRepository goLiveMovieRepository = new GoLiveMovieRepository();
 
     public void registerMovie(Movie movie){
 
       movieRepository.add(movie);
+    }
+
+    public void registerMovie2(Movie movie) throws IOException {
+        goLiveMovieRepository.add(movie);
     }
 }
